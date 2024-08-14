a99f2fa1739ac2daf73a104e1e2d9939c052ccc6
s-h-wickramarathna <sanchithaheashan655@gmail.com>
2024-08-14 11:15:32 +0530

    set netlify URL

--- functions/server.js
+++ functions/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
+const router = express.Router();
 
-// Your Express routes
-app.get('/api/hello', (req, res) => {
+
+
+router.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Express!' });
 });
 
-// Additional routes can go here
 
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 module.exports.handler = serverless(app);
