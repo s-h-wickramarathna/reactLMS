f1c6efce52ffec69cf83f7a4db441820f76e53b6
s-h-wickramarathna <sanchithaheashan655@gmail.com>
2024-08-14 11:28:38 +0530

    set Mongoose

--- functions/server.js
+++ functions/server.js
@@ -1,14 +1,38 @@
-const express = require('express');
-const serverless = require('serverless-http');
+const express = require("express");
+const mongoose = require("mongoose");
+const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 
-
-
-router.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Express!' });
+router.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Express!" });
 });
 
+router.get("/save", (req, res) => {
+    res.send("save Page");
+  });
 
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+router.get("/product", async (req, res) => {
+    try {
+      const product = await Product.find({});
+      res.status(200).json(product);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+
+mongoose
+  .connect(
+    "mongodb+srv://Heshan655:6uVyUM7q9nIBO2OC@devetaminapi.62egjtv.mongodb.net/Node-API?retryWrites=true&w=majority&appName=DevetaminAPI"
+  )
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/.netlify/functions/server", router); // path must route to lambda
 module.exports.handler = serverless(app);
---
+++ models/productModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please Enter Product Name"],
+    },
+    quentity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model('Product',productSchema);
+module.exports = Product;
