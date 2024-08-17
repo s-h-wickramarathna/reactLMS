6af47930a55402ad69a29b5b8a9a7bb76820b361
s-h-wickramarathna <sanchithaheashan655@gmail.com>
2024-08-17 13:54:35 +0530

    Mongoos required removed

--- functions/server.js
+++ functions/server.js
@@ -63,9 +63,11 @@ router.post("/student", async (req, res) => {
 // Create New Super Admin
 router.post("/superadmin", async (req, res) => {
   try {
-    console.log('Request Body:', req.body);  // Log the request body
-    const superAdmin = await SuperAdmin.create(req.body);
-    res.status(200).json(superAdmin);
+    
+    const { superAdminId, superAdminPassword, superAdminName }  = req.body;
+    res.status(200).json(superAdminId);
+    // const superAdmin = await SuperAdmin.create(req.body);
+    // res.status(200).json(superAdmin);
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: error.message });
