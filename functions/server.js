dd9008cbf329af1f10be71ce17e4ed5430be2075
s-h-wickramarathna <sanchithaheashan655@gmail.com>
2024-08-17 13:14:57 +0530

    Add Super Admin To Mongoose

--- functions/server.js
+++ functions/server.js
@@ -60,6 +60,17 @@ router.post("/student", async (req, res) => {
   }
 });
 
+// Create New Student
+router.post("/superadmin", async (req, res) => {
+  try {
+    const superAdmin = await SuperAdmin.create(req.body);
+    res.status(200).json(superAdmin);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // Fetch All Students
 router.get("/students", async (req, res) => {
   try {
